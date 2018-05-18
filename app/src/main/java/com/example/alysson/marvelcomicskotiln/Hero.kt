9cd5f7e3d50b7fad90627c4976b65dca93d7d776
alyssonr <alyssonr@dbserver.com.br>
2018-05-18 12:09:00 -0300

    [Update] Clean code

--- app/src/main/java/com/example/alysson/marvelcomicskotiln/Hero.kt
+++
@@ -1,4 +0,0 @@
-package com.example.alysson.marvelcomicskotiln
-
-class Hero (val id:Int?, val name:String, val description:String?, val comicsAvailable:Int?, val seriesAvailable:Int?,
-    val storiesAvailable:Int?, val eventsAvailable:Int?, val onlineUrl:String? , val thumbnail:String? )
